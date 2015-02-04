19916ec279119ff87ba412658bb83bce4fead9e8
zac1st1k <zacfirst@gmail.com>
2015-02-04 23:26:10 +1100

    + Setup Storyboard

---
+++ ExchangeAGram/FeedCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by Zac on 4/02/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+}
---
+++ ExchangeAGram/FeedViewController.swift
@@ -0,0 +1,51 @@
+//
+//  FeedViewController.swift
+//  ExchangeAGram
+//
+//  Created by Zac on 4/02/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+ 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    //UICollectionView Delegate
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    @IBAction func snapBarButtonItemPressed(sender: UIBarButtonItem) {
+    }
+}
